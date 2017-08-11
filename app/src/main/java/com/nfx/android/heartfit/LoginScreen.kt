2a61e7ae682257339850ff146547af7efc42ef71
Nicholas Winder <nfxdevelopment@gmail.com>
2017-08-11 15:36:12 +0100

    Added a runtime permission check to the login screen for api 21+

--- app/src/main/java/com/nfx/android/heartfit/LoginScreen.kt
+++ app/src/main/java/com/nfx/android/heartfit/LoginScreen.kt
@@ -1,8 +1,17 @@
 package com.nfx.android.heartfit
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.support.annotation.RequiresApi
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.util.Log
 import com.google.android.gms.common.ConnectionResult
 import com.nfx.android.heartfit.GoogleFitManager.ConnectionStatus
@@ -11,6 +20,7 @@ import com.nfx.android.heartfit.network.GoogleFitHeartRateInterface
 import com.nfx.android.heartfit.network.HeartRateDataInterface
 import io.reactivex.disposables.Disposable
 import javax.inject.Inject
+
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -22,6 +32,7 @@ class LoginScreen : BaseActivity() {
 
     private var authInProgress = false
     private val REQUEST_OAUTH = 1
+    private val MY_PERMISSIONS_REQUEST_BODY_SENSORS = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +40,10 @@ class LoginScreen : BaseActivity() {
         component.inject(this)
 
         setContentView(R.layout.activity_login_screen)
+    }
+
+    override fun onStart() {
+        super.onStart()
 
         connectToHeartRateInterface()
     }
@@ -36,16 +51,12 @@ class LoginScreen : BaseActivity() {
     override fun onStop() {
         super.onStop()
 
-        if (heartRateDataInterface is GoogleFitHeartRateInterface) {
-            (heartRateDataInterface as GoogleFitHeartRateInterface).disconnectFromManager()
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-
         if (googleFitConnectionListener != null) {
             (googleFitConnectionListener as Disposable).dispose()
+        }
+
+        if (heartRateDataInterface is GoogleFitHeartRateInterface) {
+            (heartRateDataInterface as GoogleFitHeartRateInterface).disconnectFromManager()
         }
     }
 
@@ -68,7 +79,7 @@ class LoginScreen : BaseActivity() {
             googleFitConnectionListener = (heartRateDataInterface as GoogleFitHeartRateInterface).getConnectionListener()
                     .subscribe { (connectionStatus, _, connectionResult) ->
                         when (connectionStatus) {
-                            ConnectionStatus.CONNECTED -> progressToMainScreen()
+                            ConnectionStatus.CONNECTED -> checkPermissions()
                             ConnectionStatus.SUSPENDED -> googleFitAccessSuspended()
                             ConnectionStatus.DISCONNECTED -> handleReconnect(connectionResult)
                         }
@@ -81,6 +92,7 @@ class LoginScreen : BaseActivity() {
     private fun progressToMainScreen() {
         val intent = Intent(this, HeartRateGraph::class.java)
         startActivity(intent)
+        finish()
     }
 
     private fun googleFitAccessSuspended() {
@@ -102,5 +114,82 @@ class LoginScreen : BaseActivity() {
                 }
             }
         }
+    }
+
+    private fun checkPermissions() {
+        if (hasPermissions()) {
+            progressToMainScreen()
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT_WATCH) {
+                requestPermissions()
+            }
+        }
+    }
+
+    private fun hasPermissions(): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT_WATCH) {
+            return ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.BODY_SENSORS) == PackageManager.PERMISSION_GRANTED
+        } else {
+            return true
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT_WATCH)
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.BODY_SENSORS),
+                MY_PERMISSIONS_REQUEST_BODY_SENSORS)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT_WATCH)
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_BODY_SENSORS -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    progressToMainScreen()
+                } else if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                    if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
+                            Manifest.permission.BODY_SENSORS)) {
+                        permissionPermanentlyDenied()
+                    } else {
+                        showRationale()
+                    }
+                }
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT_WATCH)
+    private fun showRationale() {
+        AlertDialog.Builder(this)
+                .setMessage(getString(R.string.permission_rationale))
+                .setNeutralButton(android.R.string.ok, { _, _ -> requestPermissions() })
+                .show()
+    }
+
+    private fun permissionPermanentlyDenied() {
+        AlertDialog.Builder(this)
+                .setMessage(getString(R.string.permission_rationale))
+                .setPositiveButton(getString(R.string.permissions), { _, _ -> startApplicationDetailsActivity() })
+                .setNegativeButton(android.R.string.cancel, { _, _ -> finish() })
+                .show()
+    }
+
+    private fun startApplicationDetailsActivity() {
+        try {
+            //Open the specific App Info page:
+            val intent = Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+            intent.data = Uri.parse("package:" + packageName)
+            startActivity(intent)
+
+        } catch (e: ActivityNotFoundException) {
+            //Open the generic Apps page:
+            val intent = Intent(android.provider.Settings.ACTION_MANAGE_APPLICATIONS_SETTINGS)
+            startActivity(intent)
+        }
+        finish()
     }
 }
